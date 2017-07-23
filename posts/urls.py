2348c8b061a35e8414adf3e4e01ed5955f7b31ed
kruwaih <kruwaih@live.com>
2017-07-23 20:40:08 +0300

    create a list and detail page

--- posts/urls.py
+++ posts/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     url(r'^create/$', views.post_create, name = 'create'), #when i open 127.0.0.1:8000/create     $to definde where the url end
     url(r'^update/$', views.post_update, name = 'update'), #when i open 127.0.0.1:8000/update 
     url(r'^delete/$', views.post_delete, name = 'delete'),
-    url(r'^list/$', views.post_list, name = 'list'),
-    url(r'^detail/$', views.post_detail, name = 'detail'),
+    url(r'^post_list/$', views.post_list, name = 'post_list'),
+    # url(r'^detail/$', views.post_detail, name = 'detail'),
+    url(r'^post_detail/(?P<post_id>\d+)/$', views.post_detail, name='post_detail'),
     
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from.models import Post
+from .models import Post
+from django.shortcuts import get_object_or_404
 
 def post_create(request): #what will be contain when i open 127.0.0.1:8000/home   check the urls tab
 	post_list = Post.objects.all() #check line 13 we call all the object and add it in the html create file
@@ -17,8 +18,25 @@ def post_create(request): #what will be contain when i open 127.0.0.1:8000/home 
 
 
 	}
+
 	return render (request, 'create.html', context)
 
+def post_list(request):
+	obj_list = Post.objects.all()
+	context = {
+
+	"post_list": obj_list
+	}
+	return render(request, "post_list.html", context)
+
+def post_detail(request, post_id):
+	# obj = get_object_or_404(Post, id=post_id)
+	obj = Post.objects.get(id=post_id)
+	context = {
+		'instance': obj,
+
+	}
+	return render(request, 'post_detail.html', context)
 
 def	post_update(request):
 	return render (request, 'update.html', {})
@@ -26,11 +44,11 @@ def	post_update(request):
 def	post_delete(request):
 	return render (request, 'delete.html', {})
 
-def	post_list(request):
-	return render (request, 'list.html', {})
+# def	post_list(request):
+# 	return render (request, 'list.html', {})
 
-def	post_detail(request):
-	return render (request, 'detail.html', {})
+# def	post_detail(request):
+# 	return render (request, 'detail.html', {})
 
 
 

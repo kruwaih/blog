cc9d44fb0772b38bd6978b116be58b0ec79a7a99
kruwaih <kruwaih@live.com>
2017-07-24 19:51:04 +0300

    use the object for the url

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls), #when i write the admin in the url it will open the admin page
-    url(r'^posts/', include('posts.urls')) # to access the urls in the post      ^this is to define where the url begin
+    url(r'^posts/', include('posts.urls', namespace='posts')) # to access the urls in the post      ^this is to define where the url begin	/namespace we use it if we have same url name for anothe url app
 ]

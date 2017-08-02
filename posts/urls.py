d2762ef21c367ea1cb182f32d352ded3da47df10
kruwaih <kruwaih@live.com>
2017-08-02 19:26:04 +0300

    like and button

--- posts/urls.py
+++ posts/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     url(r'^update/(?P<slug>[-\w]+)/$', views.post_update, name = 'update'), #when i open 127.0.0.1:8000/update 
     url(r'^delete/(?P<slug>[-\w]+)/$', views.post_delete, name = 'delete'),
     url(r'^list/$', views.post_list, name = 'list'),
-    # url(r'^detail/$', views.detail, name = 'detail'),
     url(r'^detail/(?P<slug>[-\w]+)/$', views.post_detail, name='detail'),
     url(r'^create/$', views.post_create, name = 'create'),
     url(r'^signup/$', views.usersignup, name = 'signup'),
     url(r'^login/$', views.userlogin, name = 'login'),
+    url(r'^like_button/(?P<post_id>[\d]+)/$', views.like_button, name='like_button'),
     
 ]
